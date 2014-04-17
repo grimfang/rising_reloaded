8b1adc98d66d50a86955640d94c08e9a81ac6cb0
fireclawthefox <fireclawthefox@gmail.com>
2014-04-17 21:19:57 +0200

    new bullet character controller

--- engine/player/player.py
+++ engine/player/player.py
@@ -17,7 +17,7 @@ import logging as log
 # Panda Engine Imports
 
 # MeoTech Imports
-from engine.config import *
+from engine.config import MODEL_DIR
 from playerPhysics import PlayerPhysics
 
 #----------------------------------------------------------------------#
@@ -31,14 +31,14 @@ class Player():
     def __init__(self, _engine, _type, _obj, _levelEgg):
         """Player Contructor"""
         print "start buidling: ", _obj, " Type: ", _type
-        
+
         # Engine
         self.engine = _engine
         self.factory = self.engine.factory
-        
+
         # Object
         self.object = _obj
-        
+
         # Get the tags from the object
         self.name = _obj.getTag("player")
         self.id = int(_obj.getTag("id"))
@@ -52,26 +52,26 @@ class Player():
         #self.jumpHeight = float(_obj.getTag("jumpHeight"))
         self.isDynamic = _obj.getTag("isDynamic")
         self.script = _obj.getTag("script")
-        
+
         # States
         self.position = _obj.getPos(_levelEgg)
         self.heading = _obj.getH(_levelEgg)
-        
+
         # Player Collision Body
         self.bulletBody = None
         self.useBasicMovement = False
-        
+
         # Run checkers
         self.setControlType()
         self.setModel()
         # TODO: Load scripts for this object...
-        
+
         # Log
         log.debug("Player Builder build: %s" % (self.name))
-    
+
     #? Needs a re-write
     def setControlType(self):
-        
+
         # Check Fps Style
         if self.control == "controlType0":
             """Add a Fps Type cam and controller"""
@@ -79,32 +79,32 @@ class Player():
             # Add the fps style camera
             self.bulletBody = PlayerPhysics.buildCharacterController(self.engine,
                         self.height, self.radius, self.position, self.heading)
-            
+
             self.useBasicMovement = True
             # camera go here..
-            
+
         # Check 3rd Person Style
         if self.control == "controlType1":
             """Add a 3rd Person view cam and controller"""
             # Add Character controller from bullet
             # Add the 3rd Person view Camera
             pass
-            
+
         # Check rpg style camera
         if self.control == "controlType2":
             """Add a rpg top down style camera"""
             # Add camera
             # add the basic controller same as fps
-            # replace this style with more options to support 
+            # replace this style with more options to support
             # point and click style movement aswell.
             pass
-            
+
         # Check side scroller type camera
         if self.control == "controlType3":
             """Add a side scroller style camera"""
             # Add a side scroller type camera
             pass
-    
+
     def setModel(self):
         """Attach the given model to the player"""
         if self.model != "":
--- engine/player/playerPhysics.py
+++ engine/player/playerPhysics.py
@@ -15,10 +15,7 @@ All Physics setups and builders
 import logging as log
 
 # Panda Engine Imports
-from panda3d.bullet import BulletCharacterControllerNode
-from panda3d.bullet import BulletCapsuleShape
-from panda3d.bullet import BulletRigidBodyNode
-from panda3d.bullet import ZUp
+from bulletCharacterController import PandaBulletCharacterController
 from panda3d.core import Vec3, BitMask32
 from direct.showbase.InputStateGlobal import inputState
 
@@ -32,47 +29,54 @@ class PlayerPhysics():
     @classmethod
     def buildCharacterController(cls, _engine, _height, _radius, _pos, _head):
         """Build a basic BulletCharacter Controller"""
-        shape = BulletCapsuleShape(_radius, _height-2 * _radius, ZUp)
-        
-        charNode = BulletCharacterControllerNode(shape, _radius, "Player")
-        np = _engine.BulletObjects["player"].attachNewNode(charNode)
-        np.setPos(_pos)
-        np.setH(_head)
-        np.setCollideMask(BitMask32.allOn())
-        
-        _engine.bulletWorld.attachCharacter(np.node())
-        return np
-    
+        np = _engine.BulletObjects["player"].attachNewNode("BCC")
+        # initialise the Bullet character controller node
+        char = PandaBulletCharacterController(
+            _engine.bulletWorld,
+            np,
+            _height,
+            _height * 0.35,
+            0.02,
+            _radius)
+        # now make the character collideable with walls and ground
+        char.setCollideMask(BitMask32.allOn())
+
+        # reparent the actor to our character nodepath, so we don't
+        # need to keep track of the actualisation ourselfe
+        #self.character.actor.reparentTo(self.char.movementParent)
+
+        # set the character to the start position of the actor,
+        # as it will hold the startposition of the active level
+        char.setPos(_pos)
+        # and set the heading to those of the actor
+        char.setH(_head)
+
+        return char
+
+
+
     @classmethod
     def doPlayerJump(cls, player):
         """Allow the player to perform a jump"""
-        
-        # set jump height, speed
-        player.setMaxJumpHeight(5.0)
-        player.setJumpSpeed(8.0)
-        player.doJump()
-        
+        player.startJump(5.0)
+
     @classmethod
-    def doPlayerCrouch(cls, player):
+    def doPlayerCrouch(cls, player, startCrouching):
         """Allow the player to perform crouch"""
-        self.crouching = not self.crouching
-        
-        sz = self.crouching and 0.6 or 1.0
-        #player.bulletBody.node().getShape().setLocalScale(Vec3(1, 1, sz))
-        
-        # Get the player nodepath
-        player.bulletBody.setScale(Vec3(1, 1, sz))# * 0.3048)
-        #player.setPos(0, 0, -1 * sz)
-    
+        if startCrouching:
+            player.startCrouch()
+        else:
+            player.stopCrouch()
+
     @classmethod
     def useBasicPlayerMovement(cls, _engine, dt):
         """This sets up a basic movement for the playercontroller"""
-        
+
         # get the player
         player = _engine.GameObjects["player"]
         speed = Vec3(0, 0, 0)
         omega = 0.0
-        
+
         if inputState.isSet('forward'): speed.setY(player.runSpeed)
         if inputState.isSet('reverse'): speed.setY(-player.runSpeed)
         if inputState.isSet('left'): speed.setX(-player.runSpeed)
@@ -81,8 +85,8 @@ class PlayerPhysics():
         if inputState.isSet('turnRight'): omega = -player.turnSpeed
         if inputState.isSet('space'): self.doPlayerJump(player.bulletBody.node())
         if inputState.isSet('ctrl'): self.doPlayerCrouch(player)
-        
+
         #player.bulletBody.node().setAngularMovement(omega)
         player.bulletBody.node().setLinearMovement(speed, True)
-        
- 
+
+
