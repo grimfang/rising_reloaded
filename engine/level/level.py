521e7c99d1a9ef926b34b7dbcae5f4d70f2f5ba8
fireclawthefox <fireclawthefox@gmail.com>
2014-05-27 22:30:25 +0200

    Fix some tag reading errors
    * convert string to bool where necessary
    * add checks for isCollisionMesh tag
    * auto-remove trailing whitespaces

--- engine/level/level.py
+++ engine/level/level.py
@@ -29,72 +29,74 @@ class Level():
     def __init__(self, _engine, _type, _obj, _levelEgg):
         """BaseLevel Constructor"""
         print "start building: ", _obj, " Type: ", _type
-        
+
         # Engine
         self.engine = _engine
         self.factory = self.engine.factory
         self.renderObjectsLevel = self.engine.RenderObjects["level"]
         self.levelEgg = _levelEgg
-        
+
         # Object
         self.object = _obj
-        
+
+        def getBool(text):
+            return text.lower() in ("true", "t")
+
         # Get the tags from the object
         self.name = _obj.getTag("level")
         self.id = int(_obj.getTag("id"))
         self.subType = _obj.getTag("subType")
-        self.isDynamic = _obj.getTag("isDynamic")
-        self.isCollisionMesh = _obj.getTag("isCollisionMesh")
-        self.isClimbable = _obj.getTag("isClimbable")
-        self.useBulletPlane = _obj.getTag("useBulletPlane")
+        self.isDynamic = getBool(_obj.getTag("isDynamic"))
+        self.isCollisionMesh = getBool(_obj.getTag("isCollisionMesh"))
+        self.isClimbable = getBool(_obj.getTag("isClimbable"))
+        self.useBulletPlane = getBool(_obj.getTag("useBulletPlane"))
         self.script = _obj.getTag("script")
-        
+
         # States
         self.position = _obj.getPos(_levelEgg)
         self.hpr = _obj.getHpr(_levelEgg)
         self.scale = _obj.getScale(_levelEgg)
-        
+
         # CollisionBody
         self.bulletBody = None
         self.groundPlane = None
-        
+
         # Run Checkers
         self.buildSubType()
-        
+
         # Log
         log.debug("Level Builder build: %s" % (self.name))
-        
-    #? This needs a re-write    
+
+    #? This needs a re-write
     def buildSubType(self):
         """Build the subType that being either wall or ground"""
-        
+
         if self.subType == "wallType":
             """Build a wall"""
-            
+
             if "col" in self.name:
                 """Build the collision body for this wall"""
                 self.bulletBody = LevelPhysics.buildTriangleMesh(self.engine,
                             self.object, self.levelEgg, 0, self.isDynamic)
                 self.bulletBody.setTag("Test", "TestingTag")
-            
+
             else:
                 self.object.reparentTo(self.renderObjectsLevel)
-        
+
         elif self.subType == "groundType":
             """Build the ground with either custom Mesh or use the plane"""
-            if self.useBulletPlane:
+            if self.useBulletPlane is True:
                 self.groundPlane = LevelPhysics.buildGroundPlane(self.engine)
-                
+
                 self.object.reparentTo(self.renderObjectsLevel)
                 self.object.setPos(self.position)
                 self.object.setHpr(self.hpr)
-            
+
             else:
-                
-                if "col" in self.name:
+                if "col" in self.name or self.isCollisionMesh:
                     self.bulletBody = LevelPhysics.buildTriangleMesh(self.engine,
                             self.object, self.levelEgg, 0, self.isDynamic)
-                
+
                 else:
                     self.object.reparentTo(self.renderObjectsLevel)
                     self.object.setPos(self.position)
